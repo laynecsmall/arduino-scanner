41cfb59a08268dd4e47a67bc54b9fbbfe9f37cd8
Layne Small <laynecsmall@gmail.com>
2017-02-15 12:30:15 +1300

    crossing detection complete

--- scanner-ard/scanner-ard.ino
+++ scanner-ard/scanner-ard.ino
@@ -1,3 +1,4 @@
+  #define DEBUG 0
   
   #define SEL0 2
   #define SEL1 3
@@ -8,8 +9,10 @@
   #define MOUT 9
   #define MIN A0
 
-int outpin, i = 0;
+int outpin, i, j = 0;
 int pwm_out = 128;
+int readings[10][10] = {0};
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -37,30 +40,58 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available()){
-    outpin = Serial.parseInt();
-    Serial.println(outpin);
-    set_mux(outpin);
-  }
-    
-  Serial.println(analogRead(MIN));
-  
-  delay(1000);
+  readings[10][10] = {0};
+  for (int i = 0; i < 10; i++){
+    readings[0][i] = read_mux_num(i);
   }
 
+  if (found_crossing(readings[0], 10, 10)){
+    Serial.println("Found crossing");
+  }
+  
+
+  if (DEBUG == 1){
+  for (int i = 0; i < 10; i++){
+    Serial.print(" ");
+    Serial.print(readings[0][i]);
+  }
+  Serial.println("");
+  }
+  
+}
+
 void set_mux(int num){
-  Serial.print("setting mux pins:");
-  Serial.print(num); 
-  Serial.print(" -  ");
+  //Serial.print("setting mux pins:");
+  //Serial.print(num); 
+  //Serial.print(" -  ");
+  digitalWrite(MOUT, HIGH);
   
   for(i = 0; i < 4; i++ ) 
   {
     int pin = i + 2;
     int obit = ((num >> i) & 1);
     digitalWrite(pin, obit);
-    Serial.print(" ");
-    Serial.print(obit);
+    //Serial.print(" ");
+    //Serial.print(obit);
   }
-  Serial.print("\n");
+  //Serial.print("\n");
+}
+
+int read_mux_num(int pad){
+  set_mux(pad);
+  delayMicroseconds(50);
+  return analogRead(MIN);
+}
+
+bool found_crossing(int crossing_array[], int x, int y){
+  for (int i = 0; i < x; i++){
+    
+      if (crossing_array[i] > 10) {
+        Serial.println(crossing_array[i]);
+        return 1;
+      }
+    
+  }
+  return 0;
 }
 
